99459bdf979838eb9c811dbe963b219aa4a6d325
Quinn Elder <qre.turing@gmail.com>
2020-07-11 11:41:14 -0600

    Add working test to stylePreference.test.js

--- code/web/src/modules/stylePreferences/tests/StylePreferences.test.js
+++ code/web/src/modules/stylePreferences/tests/StylePreferences.test.js
@@ -1,20 +1,30 @@
 import React from "react";
-import { StylePreferences } from "../StylePreferences";
-import { MemoryRouter } from "react-router-dom";
+import StylePreferences from "../StylePreferences";
+import { MemoryRouter, Router } from "react-router-dom";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
+import { createStore, compose } from 'redux'
+import { rootReducer } from '../../../setup/store'
+import { createMemoryHistory } from 'history'
 
 describe("StylePreferences", () => {
 
   it('Should', () => {
 
-    const { getAllByRole } = render(
-        <MemoryRouter>
+    const testWrapper = () => {
+      const store = createStore(rootReducer, compose())
+      const history = createMemoryHistory()
+      return render(
+      <Router history={history}>
+        <Provider store={store}>
           <StylePreferences /> 
-        </MemoryRouter>       
+        </Provider>
+      </Router>       
       )
-  
+    }
+      const {getAllByRole} = testWrapper()
+
       const altText = getAllByRole('button')
       expect(altText).toHaveLength(16)
     
