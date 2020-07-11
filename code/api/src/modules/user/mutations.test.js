9cfceab13a9c1e557e08f6b0e7c76620d31ae70b
Alex Latham <alexlthm@gmail.com>
2020-07-11 09:36:00 -0600

    Fix indentions

--- code/api/src/modules/user/mutations.test.js
+++ code/api/src/modules/user/mutations.test.js
@@ -17,21 +17,21 @@ describe("user mutations", () => {
     );
   });
 
-it ("can update a user's style summary from a style survey", async() => {
-  const data = {
-    query: `mutation {
-      userStyleSummaryUpdate(
-        id: 2,
-        styleSurvey: ["comfy", "comfy", "punk", "punk", "comfy"]
-      )
-      { styleSummary }
-    }`
-  }
+  it ("can update a user's style summary from a style survey", async() => {
+    const data = {
+      query: `mutation {
+        userStyleSummaryUpdate(
+          id: 2,
+          styleSurvey: ["comfy", "comfy", "punk", "punk", "comfy"]
+        )
+        { styleSummary }
+      }`
+    }
 
-  const response = await request(server)
-    .post('/')
-    .send(data)
-    .expect(200)
+    const response = await request(server)
+      .post('/')
+      .send(data)
+      .expect(200)
 
     expect(response.body.data.userStyleSummaryUpdate.styleSummary).toEqual("comfy, but punk")
   })
